e5db74cb97e572f29c4c53944febd2ee1c322b7f
ernestoSk13 <ernesto.sanchez.kuri@gmail.com>
2026-02-08 14:53:48 -0600

    Initial Commit

---
+++ WheresTheDotTests/WheresTheDotTests.swift
@@ -0,0 +1,17 @@
+//
+//  WheresTheDotTests.swift
+//  WheresTheDotTests
+//
+//  Created by Ernesto Sánchez Kuri on 08/02/26.
+//
+
+import Testing
+@testable import WheresTheDot
+
+struct WheresTheDotTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
