e01a236b5ebe593bc185219320eb6b8d64509fba
ernestoSk13 <ernesto.sanchez.kuri@gmail.com>
2026-03-23 22:43:33 -0600

    Adding Firebase

--- WheresTheDot/WheresTheDot.swift
+++ WheresTheDot/WheresTheDot.swift
@@ -7,9 +7,20 @@
 
 import Foundation
 import SwiftUI
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+    return true
+  }
+}
 
 @main
 struct DotsGameApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
     var body: some Scene {
         WindowGroup {
             RootView()
